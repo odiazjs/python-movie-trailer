aa37fa50d2b30fb753716596ae888488e3d72674
odiaz <odiaz.dw@gmail.com>
2017-10-08 11:23:31 -0600

    code formatting.

--- serie.py
+++ serie.py
@@ -1,7 +1,15 @@
 import media
 
+
 class Serie(media.Media):
-    def __init__(self, title, description, poster_image_url, seasons, release_date):
+    def __init__(
+        self,
+        title,
+        description,
+        poster_image_url,
+        seasons,
+        release_date
+    ):
         media.Media.__init__(self, title, poster_image_url, release_date)
         self.title = title
         self.description = description
