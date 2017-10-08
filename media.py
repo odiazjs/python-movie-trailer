2b35ea862e7d5b661c0a18dee0cf05748bda6c01
odiaz <odiaz.dw@gmail.com>
2017-10-08 11:20:48 -0600

    code formatting, Media docstring.

--- media.py
+++ media.py
@@ -1,4 +1,21 @@
 class Media():
+    """This class holds the common properties of a media type object
+
+    - **parameters**, **types**, **return** and **return types**::
+          :param title: the title of the media clip
+          :param poster_image_url: the url of the poster picture
+          :param release_date: the date that the media clip was released
+          :type title: type string
+          :type poster_image_url: type string
+          :type release_date: type string
+          :return: return media instance
+          :rtype: the return type Media class
+          
+          :Example:
+          
+          video_clip = new Media('The Video Clip', 'http://www.images.com/the_video_clip.jpeg', '01/01/2015')
+          
+    """
     def __init__(self, title, poster_image_url, release_date):
         self.title = title
         self.poster_image_url = poster_image_url
--- movie.py
+++ movie.py
@@ -1,8 +1,16 @@
 import re
 import media
 
+
 class Movie(media.Media):
-    def __init__(self, title, storyline, poster_image_url, trailer_url, release_date):
+    def __init__(
+        self,
+        title,
+        storyline,
+        poster_image_url,
+        trailer_url,
+        release_date
+    ):
         media.Media.__init__(self, title, poster_image_url, release_date)
         self.title = title
         self.storyline = storyline
@@ -10,10 +18,11 @@ class Movie(media.Media):
         self.trailer_url = self.get_youtube_embeded_url(trailer_url)
         self.release_date = release_date
 
-    def get_youtube_embeded_url(self,trailer_url):
+    def get_youtube_embeded_url(self, trailer_url):
         # Extract the youtube ID from the url
         youtube_id = re.search(r'(?<=v=)[^&#]+', trailer_url)
         youtube_id = youtube_id or re.search(r'(?<=be/)[^&#]+', trailer_url)
         trailer_youtube_id = youtube_id.group(0) if youtube_id else None
         self.youtube_id = trailer_youtube_id
-        return 'http://www.youtube.com/embed/' + trailer_youtube_id + '?autoplay=1&html5=1';
+        base_url = 'http://www.youtube.com/embed/'
+        return base_url + trailer_youtube_id + '?autoplay=1&html5=1'
